30bf0ac3def549834f2fc34928ab9b4677fbdee3
marcosadriano99 <marcosadriano99@hotmail.com>
2016-05-21 21:19:42 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function digitar(){
-	var conteudo = document.getElementById("postar").value;
+    var conteudo = document.getElementById("postar").value;
 	var tamanhoCampo = conteudo.length;
 	
 	if(tamanhoCampo > 73){
@@ -16,5 +16,14 @@ function digitar(){
 
     if(tamanhoCampo < 75 && event.keyCode === 8){
         document.getElementById("postar").style.height = "25px";
+    }
+}
+
+function quantidade(){
+    var conteudo = document.getElementById("matricula").value;
+    var tamanhoCampo = conteudo.length;
+
+    if(tamanhoCampo == 14){
+        document.getElementById("matricula").value = conteudo.toString().slice(0, 13);
     }
 }
